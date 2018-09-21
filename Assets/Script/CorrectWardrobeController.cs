e306d52963c859fc7cc25bd1b0337e61507d4991
MuhLab <MuhLab@MuhLab-11.hku.edu.tr>
2018-09-21 15:45:48 +0300

    new model which maded by hasan is added to project.

---
+++ Assets/Script/CorrectWardrobeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectWardrobeController : MonoBehaviour {
+
+	private int TotalWardrobeCount;
+	private int CorrectWardrobe;
+	// Use this for initialization
+	void Start () {
+		TotalWardrobeCount = transform.childCount;
+	}
+
+	void Update () {
+		
+	}
+
+	public void selectNewCorrectWardrobe(){
+		CorrectWardrobe = Random.Range(0, TotalWardrobeCount-1);
+	}
+
+	public GameObject getCorrectWardrobe(){
+		return transform.GetChild(CorrectWardrobe).gameObject;
+	}
+}
