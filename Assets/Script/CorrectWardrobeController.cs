865506f243543a249ed2dfd94c0c1ab14345bd8a
MuhLab <MuhLab@MuhLab-11.hku.edu.tr>
2018-09-21 16:38:21 +0300

    .

--- Assets/Script/CorrectWardrobeController.cs
+++ Assets/Script/CorrectWardrobeController.cs
@@ -20,13 +20,17 @@ public class CorrectWardrobeController : MonoBehaviour {
 	public void selectNewCorrectWardrobe(){
 		CorrectWardrobe = Random.Range(0, TotalWardrobeCount-1);
 		CorrectLight.transform.position = getCorrectWardrobe().transform.position;
-		Debug.Log (CorrectLight.transform.position);
 		CorrectLight.transform.localPosition += new Vector3 (-0.2f, 0.0f, -0.2f);
-		Debug.Log (CorrectLight.transform.position);
 		CorrectLight.enabled = false;
+		Debug.Log(getCorrectWardrobe ().GetComponent<Animator> ());
 	}
 
 	public GameObject getCorrectWardrobe(){
 		return transform.GetChild(CorrectWardrobe).gameObject;
 	}
+
+	public bool isCorrectWardrobe(GameObject gameObject){
+		return (getCorrectWardrobe () == gameObject);
+	}
+
 }
